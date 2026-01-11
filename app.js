b8ef47d521f4c7a0047e81680ec26a3dffcb30ea
Arnab Datta <dattaarnab2013@gmail.com>
2026-01-12 02:16:09 +0530

    Stateless REST API Designing using HTTP methods - GET, POST, PATCH, PUT, DELETE

--- app.js
+++ app.js
@@ -1,14 +1,107 @@
+const fs = require('fs');
 const express = require('express');
 
-const app = express();
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 const port = 8080;
+const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server!', app: 'tours' });
+// GET
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
 });
-app.post('/', (req, res) => {
-  res.send({ message: 'POST', app: 'tours' });
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+
+  // if(id > tours.length){
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  }
+});
+
+// POST
+app.post('/api/v1/tours', (req, res) => {
+  const newTourId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newTourId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+// PATCH
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  } else {
+    tour.name = req.body.name;
+    fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours),
+      (err) => {
+        res.status(200).json({
+          status: 'success',
+          message: 'updated tour',
+          data: { tour },
+        });
+      }
+    );
+  }
+});
+
+// DELETE
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  } else {
+    const udpatedTours = tours.filter((t) => t.id !== id);
+    fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(udpatedTours),
+      (err) => {
+        res.status(204).json({
+          status: 'success',
+          message: 'deleted tour',
+          data: null,
+        });
+      }
+    );
+  }
 });
 
 app.listen(port, () => {
