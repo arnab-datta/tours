f18278d3fd8da680cb80fca1ce221e1c1d4d13f1
Arnab Datta <dattaarnab2013@gmail.com>
2026-01-12 16:41:43 +0530

    user routes

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 });
 
 // 2) ROUTE HANDLERS
+// tours
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -54,7 +55,6 @@ const getTour = (req, res) => {
     });
   }
 };
-
 const createTour = (req, res) => {
   const newTourId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newTourId }, req.body);
@@ -72,7 +72,6 @@ const createTour = (req, res) => {
     }
   );
 };
-
 const updateTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((t) => t.id === id);
@@ -96,8 +95,97 @@ const updateTour = (req, res) => {
     );
   }
 };
-
 const deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  } else {
+    const udpatedTours = tours.filter((t) => t.id !== id);
+    fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(udpatedTours),
+      (err) => {
+        res.status(204).json({
+          status: 'success',
+          message: 'deleted tour',
+          data: null,
+        });
+      }
+    );
+  }
+};
+// users
+const getAllUsers = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
+};
+const getUser = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+
+  // if(id > tours.length){
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  }
+};
+const createUsers = (req, res) => {
+  const newTourId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newTourId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+const updateUser = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  } else {
+    tour.name = req.body.name;
+    fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours),
+      (err) => {
+        res.status(200).json({
+          status: 'success',
+          message: 'updated tour',
+          data: { tour },
+        });
+      }
+    );
+  }
+};
+const deleteUser = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((t) => t.id === id);
   if (!tour) {
@@ -123,6 +211,33 @@ const deleteTour = (req, res) => {
 
 // 3) ROUTES
 
+// ROUTES for Tours
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// ROUTES for Users
+app.route('/api/v1/users').get(getAllUsers).post(createUsers);
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+// 4) START SERVER
+const port = 8080;
+app.listen(port, () => {
+  // cb will be called as soon as the server start listening
+  console.log(`App running at port ${port}`);
+});
+
+/*
+@Deprecated
+// Old HTTP Methods Code
+
 // // GET
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
@@ -136,16 +251,4 @@ const deleteTour = (req, res) => {
 // // DELETE
 // app.delete('/api/v1/tours/:id', deleteTour);
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-// 4) START SERVER
-const port = 8080;
-app.listen(port, () => {
-  // cb will be called as soon as the server start listening
-  console.log(`App running at port ${port}`);
-});
+*/
