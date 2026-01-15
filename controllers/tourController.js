cadf71776931313facf9026fbe130dc9269f3ef0
Arnab Datta <dattaarnab2013@gmail.com>
2026-01-15 18:35:04 +0530

    limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,6 +27,14 @@ const getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // Filed limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     const tours = await query;
     // const tours = await Tour.find().where('duration').equals(5);
 
