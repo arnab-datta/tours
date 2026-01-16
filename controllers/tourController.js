6ba2e93e0d195b6c442742d71468a5c7d55b844c
Arnab Datta <dattaarnab2013@gmail.com>
2026-01-16 20:32:59 +0530

    Refactoring API Features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 const aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -10,50 +11,13 @@ const aliasTopTours = (req, res, next) => {
 // Handlers
 const getAllTours = async (req, res) => {
   try {
-    // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // console.log(req.query, queryObj);
-    // console.log(queryObj);
-
-    // 1B) Advance Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
-
-    // console.log(JSON.parse(queryStr));
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) SORT
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Filed limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error(`This page doesn't exist`);
-    }
     // EXECUTE QUERY
-    const tours = await query;
-    // const tours = await Tour.find().where('duration').equals(5);
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
@@ -138,6 +102,71 @@ const deleteTour = async (req, res) => {
     });
   }
 };
+
+/*******************  old codes before refractoring *********************************/
+
+// const getAllTours = async (req, res) => {
+//   try {
+//     // 1A) Filtering
+//     const queryObj = { ...req.query };
+//     const excludedFields = ['page', 'sort', 'limit', 'fields'];
+//     excludedFields.forEach((el) => delete queryObj[el]);
+
+//     // console.log(req.query, queryObj);
+//     // console.log(queryObj);
+
+//     // 1B) Advance Filtering
+//     let queryStr = JSON.stringify(queryObj);
+//     queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
+
+//     // console.log(JSON.parse(queryStr));
+//     let query = Tour.find(JSON.parse(queryStr));
+
+//     // 2) SORT
+//     if (req.query.sort) {
+//       const sortBy = req.query.sort.split(',').join(' ');
+//       query = query.sort(sortBy);
+//     } else {
+//       query = query.sort('-createdAt');
+//     }
+
+//     // 3) Filed limiting
+//     if (req.query.fields) {
+//       const fields = req.query.fields.split(',').join(' ');
+//       query = query.select(fields);
+//     } else {
+//       query = query.select('-__v');
+//     }
+
+//     // 4) Pagination
+//     const page = req.query.page * 1 || 1;
+//     const limit = req.query.limit * 1 || 100;
+//     const skip = (page - 1) * limit;
+//     query = query.skip(skip).limit(limit);
+
+//     if (req.query.page) {
+//       const numTours = await Tour.countDocuments();
+//       if (skip >= numTours) throw new Error(`This page doesn't exist`);
+//     }
+
+//     // EXECUTE QUERY
+//     const tours = await query;
+
+//     // const tours = await Tour.find().where('duration').equals(5);
+
+//     res.status(200).json({
+//       status: 'success',
+//       requestedAt: req.requestTime,
+//       results: tours.length,
+//       data: { tours },
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'Failed',
+//       message: err.message,
+//     });
+//   }
+// };
 
 // Handlers using updating local file
 // const fs = require('fs');
