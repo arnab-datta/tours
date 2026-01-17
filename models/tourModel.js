883111c62f67a94e99bb91b20027876fa4a24fe9
Arnab Datta <dattaarnab2013@gmail.com>
2026-01-17 10:59:54 +0530

    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -50,6 +50,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -70,6 +74,19 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+// query middleware
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} ms`);
+  //console.log(docs);
+  next();
+});
 
 // virtual properties
 tourSchema.virtual('durationWeeks').get(function () {
