c3f48eeb94feff326f7b5493222a1d0848852a55
Arnab Datta <dattaarnab2013@gmail.com>
2026-01-17 10:37:10 +0530

    Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -55,6 +57,21 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+// momgoose document middleware: rund before .save() and .creat()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+// tourSchema.pre('save', function (next) {
+//   console.log('will save document...');
+//   next();
+// });
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// virtual properties
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
