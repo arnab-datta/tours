93d83c11127778d3da38f0815f04836f95b3525c
Arnab Datta <dattaarnab2013@gmail.com>
2026-01-19 04:14:51 +0530

    Setting Security HTTP Headers using helmet

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
+const AppError = require('./utils/appError');
 const globalErrorHanlder = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const AppError = require('./utils/appError');
 
 const app = express();
 
 // 1) Middlewares
-
-// Body Parsing: body parser alternative
-app.use(express.json());
 
 // Logging: morgan middleware
 if (process.env.NODE_ENV === 'development') {
@@ -24,8 +23,13 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-
 app.use('/api', limiter);
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
 // Serving Static Files
 app.use(express.static(`${__dirname}/public`));
@@ -36,16 +40,13 @@ app.use((req, res, next) => {
   next();
 });
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 2) ROUTE HANDLERS --> controllers
-// moved inside controllers
-
 // 3) ROUTES --> routes
-// moved inside routes folders
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
